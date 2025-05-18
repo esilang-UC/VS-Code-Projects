3396603840c601eb5b53f377b22133ccdb0a4bc9
Elijah Silang <esilang17279@ucumberlands.edu>
2025-05-18 16:24:30 -0500

    update implementation to include cases of one or less elements

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,7 +2,12 @@
 # Insertion sort is iteratively inserting an element into its correct position within a sorted section of the list.
 
 def insertionSort(array):
-    for x in range(1, len(array)):
+    arrayLength = len(array)
+
+    if arrayLength <= 1:
+        return array
+
+    for x in range(1, arrayLength):
         elem = array[x]
 
         y = x - 1
@@ -17,5 +22,12 @@ def insertionSort(array):
 
 # Array to be sorted
 array = [6, 2, 3, 5, 7, 9, 1, 8]
+print("array to be sorted: " + str(array))
 # Print array
-print(insertionSort(array))
+print("sorted array: " + str(insertionSort(array)))
+
+# Test case of 1 or less elements in array for insertion sort
+newArray = [3]
+print("new array with one or less element to be sorted: " + str(newArray))
+# Print array
+print("new array sorted: " + str(insertionSort(newArray)))
